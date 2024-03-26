f8daf2cc2a58d674e97673fd187805256367d358
bchatelard <bchatelard@users.noreply.github.com>
2024-03-26 00:20:44 +0000

    [create-pull-request] automated change

---
+++ api/v1/koyeb/docs/HasUnpaidInvoicesReply.md
@@ -0,0 +1,56 @@
+# HasUnpaidInvoicesReply
+
+## Properties
+
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**HasUnpaidInvoices** | Pointer to **bool** |  | [optional] 
+
+## Methods
+
+### NewHasUnpaidInvoicesReply
+
+`func NewHasUnpaidInvoicesReply() *HasUnpaidInvoicesReply`
+
+NewHasUnpaidInvoicesReply instantiates a new HasUnpaidInvoicesReply object
+This constructor will assign default values to properties that have it defined,
+and makes sure properties required by API are set, but the set of arguments
+will change when the set of required properties is changed
+
+### NewHasUnpaidInvoicesReplyWithDefaults
+
+`func NewHasUnpaidInvoicesReplyWithDefaults() *HasUnpaidInvoicesReply`
+
+NewHasUnpaidInvoicesReplyWithDefaults instantiates a new HasUnpaidInvoicesReply object
+This constructor will only assign default values to properties that have it defined,
+but it doesn't guarantee that properties required by API are set
+
+### GetHasUnpaidInvoices
+
+`func (o *HasUnpaidInvoicesReply) GetHasUnpaidInvoices() bool`
+
+GetHasUnpaidInvoices returns the HasUnpaidInvoices field if non-nil, zero value otherwise.
+
+### GetHasUnpaidInvoicesOk
+
+`func (o *HasUnpaidInvoicesReply) GetHasUnpaidInvoicesOk() (*bool, bool)`
+
+GetHasUnpaidInvoicesOk returns a tuple with the HasUnpaidInvoices field if it's non-nil, zero value otherwise
+and a boolean to check if the value has been set.
+
+### SetHasUnpaidInvoices
+
+`func (o *HasUnpaidInvoicesReply) SetHasUnpaidInvoices(v bool)`
+
+SetHasUnpaidInvoices sets HasUnpaidInvoices field to given value.
+
+### HasHasUnpaidInvoices
+
+`func (o *HasUnpaidInvoicesReply) HasHasUnpaidInvoices() bool`
+
+HasHasUnpaidInvoices returns a boolean if a field has been set.
+
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
+
--- api/v1/koyeb/model_verify_docker_image_reply.go
+++ api/v1/koyeb/model_verify_docker_image_reply.go
@@ -18,6 +18,7 @@ import (
 type VerifyDockerImageReply struct {
 	Success *bool `json:"success,omitempty"`
 	Reason *string `json:"reason,omitempty"`
+	Code *VerifyDockerImageReplyErrCode `json:"code,omitempty"`
 }
 
 // NewVerifyDockerImageReply instantiates a new VerifyDockerImageReply object
@@ -26,6 +27,8 @@ type VerifyDockerImageReply struct {
 // will change when the set of required properties is changed
 func NewVerifyDockerImageReply() *VerifyDockerImageReply {
 	this := VerifyDockerImageReply{}
+	var code VerifyDockerImageReplyErrCode = VERIFYDOCKERIMAGEREPLYERRCODE_UNKNOWN
+	this.Code = &code
 	return &this
 }
 
@@ -34,6 +37,8 @@ func NewVerifyDockerImageReply() *VerifyDockerImageReply {
 // but it doesn't guarantee that properties required by API are set
 func NewVerifyDockerImageReplyWithDefaults() *VerifyDockerImageReply {
 	this := VerifyDockerImageReply{}
+	var code VerifyDockerImageReplyErrCode = VERIFYDOCKERIMAGEREPLYERRCODE_UNKNOWN
+	this.Code = &code
 	return &this
 }
 
@@ -101,6 +106,38 @@ func (o *VerifyDockerImageReply) SetReason(v string) {
 	o.Reason = &v
 }
 
+// GetCode returns the Code field value if set, zero value otherwise.
+func (o *VerifyDockerImageReply) GetCode() VerifyDockerImageReplyErrCode {
+	if o == nil || isNil(o.Code) {
+		var ret VerifyDockerImageReplyErrCode
+		return ret
+	}
+	return *o.Code
+}
+
+// GetCodeOk returns a tuple with the Code field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *VerifyDockerImageReply) GetCodeOk() (*VerifyDockerImageReplyErrCode, bool) {
+	if o == nil || isNil(o.Code) {
+    return nil, false
+	}
+	return o.Code, true
+}
+
+// HasCode returns a boolean if a field has been set.
+func (o *VerifyDockerImageReply) HasCode() bool {
+	if o != nil && !isNil(o.Code) {
+		return true
+	}
+
+	return false
+}
+
+// SetCode gets a reference to the given VerifyDockerImageReplyErrCode and assigns it to the Code field.
+func (o *VerifyDockerImageReply) SetCode(v VerifyDockerImageReplyErrCode) {
+	o.Code = &v
+}
+
 func (o VerifyDockerImageReply) MarshalJSON() ([]byte, error) {
 	toSerialize := map[string]interface{}{}
 	if !isNil(o.Success) {
@@ -108,6 +145,9 @@ func (o VerifyDockerImageReply) MarshalJSON() ([]byte, error) {
 	}
 	if !isNil(o.Reason) {
 		toSerialize["reason"] = o.Reason
+	}
+	if !isNil(o.Code) {
+		toSerialize["code"] = o.Code
 	}
 	return json.Marshal(toSerialize)
 }
