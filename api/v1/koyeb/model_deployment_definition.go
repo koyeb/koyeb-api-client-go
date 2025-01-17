b6cc06b231c481f55e4869d07b4a3a2f8d14e66e
lukaszo <lukaszo@users.noreply.github.com>
2025-01-17 14:30:35 +0000

    [create-pull-request] automated change

--- api/v1/koyeb/model_deployment_definition.go
+++ api/v1/koyeb/model_deployment_definition.go
@@ -27,7 +27,7 @@ type DeploymentDefinition struct {
 	InstanceTypes []DeploymentInstanceType `json:"instance_types,omitempty"`
 	HealthChecks []DeploymentHealthCheck `json:"health_checks,omitempty"`
 	Volumes []DeploymentVolume `json:"volumes,omitempty"`
-	Files []File `json:"files,omitempty"`
+	ConfigFiles []ConfigFile `json:"config_files,omitempty"`
 	SkipCache *bool `json:"skip_cache,omitempty"`
 	Docker *DockerSource `json:"docker,omitempty"`
 	Git *GitSource `json:"git,omitempty"`
@@ -408,36 +408,36 @@ func (o *DeploymentDefinition) SetVolumes(v []DeploymentVolume) {
 	o.Volumes = v
 }
 
-// GetFiles returns the Files field value if set, zero value otherwise.
-func (o *DeploymentDefinition) GetFiles() []File {
-	if o == nil || isNil(o.Files) {
-		var ret []File
+// GetConfigFiles returns the ConfigFiles field value if set, zero value otherwise.
+func (o *DeploymentDefinition) GetConfigFiles() []ConfigFile {
+	if o == nil || isNil(o.ConfigFiles) {
+		var ret []ConfigFile
 		return ret
 	}
-	return o.Files
+	return o.ConfigFiles
 }
 
-// GetFilesOk returns a tuple with the Files field value if set, nil otherwise
+// GetConfigFilesOk returns a tuple with the ConfigFiles field value if set, nil otherwise
 // and a boolean to check if the value has been set.
-func (o *DeploymentDefinition) GetFilesOk() ([]File, bool) {
-	if o == nil || isNil(o.Files) {
+func (o *DeploymentDefinition) GetConfigFilesOk() ([]ConfigFile, bool) {
+	if o == nil || isNil(o.ConfigFiles) {
     return nil, false
 	}
-	return o.Files, true
+	return o.ConfigFiles, true
 }
 
-// HasFiles returns a boolean if a field has been set.
-func (o *DeploymentDefinition) HasFiles() bool {
-	if o != nil && !isNil(o.Files) {
+// HasConfigFiles returns a boolean if a field has been set.
+func (o *DeploymentDefinition) HasConfigFiles() bool {
+	if o != nil && !isNil(o.ConfigFiles) {
 		return true
 	}
 
 	return false
 }
 
-// SetFiles gets a reference to the given []File and assigns it to the Files field.
-func (o *DeploymentDefinition) SetFiles(v []File) {
-	o.Files = v
+// SetConfigFiles gets a reference to the given []ConfigFile and assigns it to the ConfigFiles field.
+func (o *DeploymentDefinition) SetConfigFiles(v []ConfigFile) {
+	o.ConfigFiles = v
 }
 
 // GetSkipCache returns the SkipCache field value if set, zero value otherwise.
@@ -635,8 +635,8 @@ func (o DeploymentDefinition) MarshalJSON() ([]byte, error) {
 	if !isNil(o.Volumes) {
 		toSerialize["volumes"] = o.Volumes
 	}
-	if !isNil(o.Files) {
-		toSerialize["files"] = o.Files
+	if !isNil(o.ConfigFiles) {
+		toSerialize["config_files"] = o.ConfigFiles
 	}
 	if !isNil(o.SkipCache) {
 		toSerialize["skip_cache"] = o.SkipCache
